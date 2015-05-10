956c976dfd1765bc2fb36e078d5c716e9d3d527e
udaltsov <udaltsov-dmitriy@yandex.ru>
2015-05-10 23:25:22 +0300

    JUNAUT-1 #done

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ class IndexView(generic.ListView):
     context_object_name = 'objects_list'
     model = TreeObject
 
+
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
         context['interfaces'] = Interface.objects.all()
