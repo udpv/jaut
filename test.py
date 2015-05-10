7604e1d3b6758087cff05967d5ce1cb6c17ce1eb
udaltsov <udaltsov-dmitriy@yandex.ru>
2015-05-10 23:23:06 +0300

    JUNAUT-1 #resolve

--- test.py
+++ test.py
@@ -4,6 +4,7 @@ class IndexView(generic.ListView):
     template_name = 'network/index.html'
     context_object_name = 'objects_list'
     model = TreeObject
+
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
         context['interfaces'] = Interface.objects.all()
