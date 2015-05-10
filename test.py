0d01955e57d83ded43a3e0c9f0108a3756d79106
udaltsov <udaltsov-dmitriy@yandex.ru>
2015-05-10 23:27:15 +0300

    JUNAUT-1 #comment "hey hjhjh hj hjh jhj hjh jh j"

--- test.py
+++ test.py
@@ -8,6 +8,9 @@ class IndexView(generic.ListView):
 
 
 
+
+
+
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
         context['interfaces'] = Interface.objects.all()
